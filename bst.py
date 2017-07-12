67c386063d8faa4e28d9d886468c75a5a60f2956
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2017-07-12 20:26:05 +0530

    insert iterative for bst

--- bst.py
+++ bst.py
@@ -10,3 +10,23 @@ class BST:
             self.root = None
         else:
             self.root = BSTNode(data)
+
+    def insert(self, data):
+        new_node = BSTNode(data)
+        if self.root is None:
+            self.root = new_node
+        else:
+            temp = self.root
+            while temp is not None:
+                if data <= temp.data:
+                    if temp.left is None:
+                        temp.left = new_node
+                        break
+                    else:
+                        temp = temp.left
+                else:
+                    if temp.right is None:
+                        temp.right = new_node
+                        break
+                    else:
+                        temp = temp.right
