65a0fb441c60d4f6622a506a0f95eff7c4446895
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2017-07-16 02:08:17 +0530

    LinkedList init, search, insert implemented in find_loop

---
+++ loop_in_linked_list.py
@@ -0,0 +1,40 @@
+# Find if the linkedlsit have any loop or not.
+# If yes, length of loop.
+# Intersection point.
+
+class Node:
+    def __init__(self, data, nex=None):
+        self.data = data
+        self.nex = nex
+
+class LinkedList:
+    def __init__(self, data=None):
+        if data is None:
+            self.root = None
+        else:
+            self.root = Node(data)
+
+    def insert(self, data):
+        new_node = Node(data)
+        temp = self.root
+        if temp is None:
+            self.root = new_node
+        while temp.nex is not None:
+            temp = temp.nex
+        temp.nex = new_node
+
+    def search(self, data):
+        temp = self.root
+        if temp is None:
+            return False
+        while temp is not None:
+            if temp.data == data:
+                return True
+            temp = temp.nex
+        return False
+
+
+
+if __name__ == '__main__':
+    a = LinkedList(9)
+    a.pprint()
